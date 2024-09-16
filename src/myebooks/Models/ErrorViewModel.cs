595fa12e894e5508f2927aa0b425f1abdd11b83f
agilematrix <agilematrix@users.noreply.github.com>
2024-09-16 00:17:52 -0400

    Initial commit

---
+++ src/myebooks/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace myebooks.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
